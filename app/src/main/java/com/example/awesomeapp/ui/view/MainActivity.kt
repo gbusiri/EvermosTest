122e7ace0bda599e469ad1768e3a8b47ffdb1ea7
Moch Ginanjar Busiri <gbusiri@gmail.com>
2021-06-16 00:54:16 +0700

    implement pagination

--- app/src/main/java/com/example/awesomeapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/awesomeapp/ui/view/MainActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.awesomeapp.R
 import com.example.awesomeapp.data.model.PhotoModel
@@ -15,7 +17,6 @@ import com.example.awesomeapp.ui.adapter.CuratedAdapter
 import com.example.awesomeapp.ui.intent.MainIntent
 import com.example.awesomeapp.ui.viewmodel.MainViewModel
 import com.example.awesomeapp.ui.viewstate.MainState
-import com.example.awesomeapp.util.Constants
 import com.example.awesomeapp.util.ViewModelFactory
 import com.google.android.material.appbar.AppBarLayout
 import com.mindorks.framework.mvi.data.api.ApiHelperImpl
@@ -24,13 +25,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlin.math.abs
+import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainViewModel: MainViewModel
     private var currentPage = 0
     private var isGridView = false
-    private var photos = listOf<PhotoModel>()
+    private var isLoading = true
+    private var photos = mutableListOf<PhotoModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,7 +65,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
         invalidateOptionsMenu()
-        setupAdapter()
+        setupRecyclerView()
         return true
     }
 
@@ -80,12 +83,21 @@ class MainActivity : AppCompatActivity() {
             mainViewModel.state.collect {
                 when (it) {
                     is MainState.Idle -> {} // do nothing
-                    is MainState.CuratedLoading -> {}
-                    is MainState.Curated -> {
-                        photos = it.data.photos
-                        setupAdapter()
+                    is MainState.CuratedLoading -> {
+                        isLoading = true
+                        loading.visibility = View.VISIBLE
                     }
-                    is MainState.Error -> {}
+                    is MainState.Curated -> {
+                        isLoading = false
+                        loading.visibility = View.GONE
+                        photos.addAll(it.data.photos)
+                        setupRecyclerView()
+                    }
+                    is MainState.Error -> {
+                        isLoading = false
+                        loading.visibility = View.GONE
+                        Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
@@ -124,7 +136,7 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun setupAdapter() {
+    private fun setupRecyclerView() {
         val curatedAdapter = CuratedAdapter(photos, isGridView) {
             goToDetailScreen()
         }
@@ -133,6 +145,21 @@ class MainActivity : AppCompatActivity() {
                                 else LinearLayoutManager(this@MainActivity)
             adapter = curatedAdapter
         }
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                val lastVisibleItem = if (isGridView) {
+                    val lastItemPositions = (recyclerView.layoutManager as StaggeredGridLayoutManager).findLastVisibleItemPositions(null)
+                    if (lastItemPositions.isNotEmpty()) max(lastItemPositions[0], lastItemPositions[1]) else 0
+                } else {
+                    (recyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+                }
+                if (isLoading.not() && recyclerView.layoutManager?.itemCount == lastVisibleItem + 1) {
+                    fetchImages()
+                }
+            }
+        })
     }
 
     private fun goToDetailScreen() {
